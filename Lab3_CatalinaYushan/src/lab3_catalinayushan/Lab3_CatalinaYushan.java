43fc5eba134969f04ab0d4b9f7f64a56359b184a
CatalinaYushan <115840690+CatalinaYushan@users.noreply.github.com>
2022-10-28 16:58:18 -0600

    Add files via upload

---
+++ Lab3_CatalinaYushan/src/lab3_catalinayushan/Lab3_CatalinaYushan.java
@@ -0,0 +1,56 @@
+package lab3_catalinayushan;
+
+import java.util.Scanner;
+
+public class Lab3_CatalinaYushan {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        boolean salir = false;
+        int choice;
+        
+        while(!salir){
+     
+        System.out.println("----Bienvenido!----");
+        System.out.println("1. Ejercicio Cadena Repetida ");
+        System.out.println("2. Ejercicio Sumatoria");
+        System.out.println("3. Ejercicio Reloj");
+        System.out.println("4. Salir");
+        
+        System.out.println("Ingrese una de las opciones: ");
+        choice = input.nextInt();
+        
+        //Ejercicio #1
+        switch (choice){
+            case 1:
+        String sentence;
+        char [] secret;
+        System.out.println("Ingrese una oracion con la estructura 'El..Barca...juega..los.jueves...': ");
+        sentence = input.nextLine();
+        sentence = sentence.toLowerCase();
+        secret = sentence.toCharArray();
+        
+        
+        //Ejercicio #2
+        Scanner = new Scanner (System.in);
+        
+        System.out.println("Ingrese el valor de K: ");
+        
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+        
+        
+    }
+    
+}
